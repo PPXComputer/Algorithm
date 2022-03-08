2285db993b612635e3332cc5fedae8ae2195b6ec
PPXComputer <3226824851@qq.com>
2022-03-08 19:18:53 +0800

    取消重复的节点结构

--- include/LeetCode200.h
+++ include/LeetCode200.h
@@ -3,6 +3,7 @@
 class LeetCode200
 {
 	static void medium_33();
+
 	static void medium_34();
 
 	//. ËÑË÷¶þÎ¬¾ØÕó
@@ -57,44 +58,31 @@ private:
 
 		std::vector<ListNode*> node = {};
 	};
-		static std::unique_ptr<ListNode> new_list(size_t length) {
-			assert(length != 0);
-			auto result = std::make_unique<ListNode>(0);
-			result->node.reserve(length - 1);
-			auto root = result.get();
-			for (size_t i = 1; i < length; i++)
-			{
-				root->next = new ListNode(i);
-				root = root->next;
-				result->node.emplace_back(root);
-			}
-			return result;
+	static std::unique_ptr<ListNode> new_list(size_t length) {
+		assert(length != 0);
+		auto result = std::make_unique<ListNode>(0);
+		result->node.reserve(length - 1);
+		auto root = result.get();
+		for (size_t i = 1; i < length; i++)
+		{
+			root->next = new ListNode(i);
+			root = root->next;
+			result->node.emplace_back(root);
 		}
-
-		static std::unique_ptr<ListNode> new_list(const std::vector<int>& data) {
-			if (data.empty())return nullptr;
-			auto result = std::make_unique< ListNode>(data[0]);
-			auto root = result.get();
-			result->node.reserve(data.size() - 1);
-			size_t size = data.size();
-			for (size_t i = 1; i < size; i++)
-			{
-				root->next = new ListNode(data[i]);
-				root = root->next;
-				result->node.emplace_back(root);
-			}
-			return result;
+		return result;
+	}
+	static std::unique_ptr<ListNode> new_list(const std::vector<int>& data) {
+		if (data.empty())return nullptr;
+		auto result = std::make_unique< ListNode>(data[0]);
+		auto root = result.get();
+		result->node.reserve(data.size() - 1);
+		size_t size = data.size();
+		for (size_t i = 1; i < size; i++)
+		{
+			root->next = new ListNode(data[i]);
+			root = root->next;
+			result->node.emplace_back(root);
 		}
-		~ListNode() {
-
-			for (auto data : node) {
-				delete data;
-			}
-		}
-	private:
-
-		std::vector<ListNode*> node = {};
-	};
-
-
+		return result;
+	}
 };
